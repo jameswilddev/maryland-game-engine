7f8a84bfc586180f358aeeda3494f66d1da8864e
jameswilddev <jameswilddev@outlook.com>
2022-07-25 00:52:14 +0100

    Implement colors.

---
+++ lib/data/primitives/color.dart
@@ -0,0 +1,64 @@
+import 'package:maryland_game_engine/data/primitives/u8.dart';
+
+/// A 32-bits-per-pixel [Color], including [red], [green] and [blue] intensities
+/// (where 0 = minimum intensity and 255 = maximum intensity) and a
+/// (non-pre-multiplied) [opacity] (where 0 = transparent and 255 = opaque).
+class Color {
+  /// The intensity of the red channel (where 0 = minimum intensity and 255 =
+  /// maximum intensity, non-pre-multiplied).
+  final U8 red;
+
+  /// The intensity of the green channel (where 0 = minimum intensity and 255 =
+  /// maximum intensity, non-pre-multiplied).
+  final U8 green;
+
+  /// The intensity of the blue channel (where 0 = minimum intensity and 255 =
+  /// maximum intensity, non-pre-multiplied).
+  final U8 blue;
+
+  /// The opacity of the overall [Color], where 0 = transparent and
+  /// 255 = opaque.
+  final U8 opacity;
+
+  /// Creates a new 32-bits-per-pixel [Color], including [red], [green] and [blue]
+  /// intensities (where 0 = minimum intensity and 255 = maximum intensity) and
+  /// a (non-pre-multiplied) [opacity] (where 0 = transparent and 255 = opaque).
+  /// Throws a [RangeError] when any argument is not a valid U8.
+  Color(this.red, this.green, this.blue, this.opacity) {
+    validateU8(red, "Red intensity of color");
+    validateU8(green, "Green intensity of color");
+    validateU8(blue, "Blue intensity of color");
+    validateU8(opacity, "Opacity of color");
+  }
+
+  /// Serializes this [EntityId] to a series of [U8]s describing it.
+  Iterable<U8> serialize() sync* {
+    yield* serializeU8(red, "Color red intensity");
+    yield* serializeU8(green, "Color green intensity");
+    yield* serializeU8(blue, "Color blue intensity");
+    yield* serializeU8(opacity, "Color opacity");
+  }
+
+  /// Deserializes a [Color] from a series of [U8]s describing one, instead
+  /// throwing a [StateError] including the given [description] should the given
+  /// [iterator] not contain enough [U8]s to fully describe one, or a
+  /// [RangeError] including the given [description] should the iterator contain
+  /// invalid [U8]s.
+  static Color deserialize(Iterator<U8> iterator, String description) {
+    final red = deserializeU8(iterator, "$description (red intensity)");
+    final green = deserializeU8(iterator, "$description (green intensity)");
+    final blue = deserializeU8(iterator, "$description (blue intensity)");
+    final opacity = deserializeU8(iterator, "$description (opacity)");
+
+    return Color(red, green, blue, opacity);
+  }
+
+  @override
+  bool operator == (other) => other is Color && red == other.red && green == other.green && blue == other.blue && opacity == other.opacity;
+
+  @override
+  int get hashCode => red << 24 | green << 16 | blue << 8 | opacity;
+
+  @override
+  String toString() => "#${red.toRadixString(16).padLeft(2, "0")}${green.toRadixString(16).padLeft(2, "0")}${blue.toRadixString(16).padLeft(2, "0")}${opacity.toRadixString(16).padLeft(2, "0")}";
+}
---
+++ test/data/primitives/color_test.dart
@@ -0,0 +1,154 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:maryland_game_engine/data/primitives/color.dart';
+import 'package:maryland_game_engine/data/primitives/u8.dart';
+
+void main() {
+  test("equal", () {
+    const red = 0xe6;
+    const green = 0x03;
+    const blue = 0x48;
+    const opacity = 0x36;
+
+    final a = Color(red, green, blue, opacity);
+    final b = Color(red, green, blue, opacity);
+
+    expect(a.red, equals(red));
+    expect(a.green, equals(green));
+    expect(a.blue, equals(blue));
+    expect(a.opacity, equals(opacity));
+    expect(a, equals(b));
+    expect(a.hashCode, equals(b.hashCode));
+    expect(a.toString(), equals("#e6034836"));
+    expect(a.serialize(), orderedEquals([0xe6, 0x03, 0x48, 0x36]));
+  });
+
+  test("unequal red", () {
+    const green = 0x03;
+    const blue = 0x48;
+    const opacity = 0x36;
+
+    final a = Color(0xce, green, blue, opacity);
+    final b = Color(0x5a, green, blue, opacity);
+
+    expect(a, isNot(equals(b)));
+    expect(a.hashCode, isNot(equals(b.hashCode)));
+  });
+
+  test("unequal green", () {
+    const red = 0xe6;
+    const blue = 0x48;
+    const opacity = 0x36;
+
+    final a = Color(red, 0xce, blue, opacity);
+    final b = Color(red, 0x5a, blue, opacity);
+
+    expect(a, isNot(equals(b)));
+    expect(a.hashCode, isNot(equals(b.hashCode)));
+  });
+
+  test("unequal blue", () {
+    const red = 0xe6;
+    const green = 0x03;
+    const opacity = 0x36;
+
+    final a = Color(red, green, 0xce, opacity);
+    final b = Color(red, green, 0x5a, opacity);
+
+    expect(a, isNot(equals(b)));
+    expect(a.hashCode, isNot(equals(b.hashCode)));
+  });
+
+  test("unequal opacity", () {
+    const red = 0xe6;
+    const green = 0x03;
+    const blue = 0x48;
+
+    final a = Color(red, green, blue, 0xce);
+    final b = Color(red, green, blue, 0x5a);
+
+    expect(a, isNot(equals(b)));
+    expect(a.hashCode, isNot(equals(b.hashCode)));
+  });
+
+  group("deserialize", () {
+    test("throws the expected exception when the iterator ends immediately",
+        () {
+      expect(
+          () => Color.deserialize(<U8>[].iterator, "Example Description"),
+          throwsA(predicate((e) =>
+              e is StateError &&
+              e.message == "Example Description (red intensity) - Unexpected end of file.")));
+    });
+
+    test("throws the expected exception when the iterator ends after one U8",
+        () {
+      expect(
+          () => Color.deserialize(<U8>[0xce].iterator, "Example Description"),
+          throwsA(predicate((e) =>
+              e is StateError &&
+              e.message == "Example Description (green intensity) - Unexpected end of file.")));
+    });
+
+    test("throws the expected exception when the iterator ends after two U8s",
+        () {
+      expect(
+          () =>
+              Color.deserialize(<U8>[0xce, 0x80].iterator, "Example Description"),
+          throwsA(predicate((e) =>
+              e is StateError &&
+              e.message == "Example Description (blue intensity) - Unexpected end of file.")));
+    });
+
+    test("throws the expected exception when the iterator ends after three U8s",
+        () {
+      expect(
+          () => Color.deserialize(
+              <U8>[0xce, 0x80, 0xd9].iterator, "Example Description"),
+          throwsA(predicate((e) =>
+              e is StateError &&
+              e.message == "Example Description (opacity) - Unexpected end of file.")));
+    });
+
+    test("throws the expected exception when the iterator includes invalid U8s",
+        () {
+      expect(
+          () => Color.deserialize(
+              <U8>[0xce, 0x80, 300, 0xd9].iterator, "Example Description"),
+          throwsA(predicate((e) =>
+              e is RangeError &&
+              e.message ==
+                  "Example Description (blue intensity) - Value is out of range for a U8 (greater than 255).")));
+    });
+
+    test("returns the expected color when the iterable ends", () {
+      expect(
+          Color.deserialize(
+              [0xce, 0x80, 0xd9, 0xe8].iterator, "Example Description"),
+          equals(Color(0xce, 0x80, 0xd9, 0xe8)));
+    });
+
+    group("when the iterable does not end", () {
+      Iterator<U8> iterator = <U8>[].iterator;
+      Color output = Color(0, 0, 0, 0);
+
+      setUpAll(() {
+        iterator = [0xce, 0x80, 0xd9, 0xe8, 0x48, 0x07, 0xe5, 0x6e].iterator;
+        output = Color.deserialize(iterator, "Example Description");
+      });
+
+      test("returns the expected color", () {
+        expect(output, equals(Color(0xce, 0x80, 0xd9, 0xe8)));
+      });
+
+      test("leaves the remaining U8s un-iterated", () {
+        final remaining = <U8>[];
+
+        while (iterator.moveNext()) {
+          remaining.add(iterator.current);
+        }
+
+        expect(remaining, orderedEquals([0x48, 0x07, 0xe5, 0x6e]));
+      });
+    });
+  });
+}
