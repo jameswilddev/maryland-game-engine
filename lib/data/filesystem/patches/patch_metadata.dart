db2feb2ceed5ba2b55a8b0ba3b0e0847586e8be9
jameswilddev <jameswilddev@outlook.com>
2022-07-28 10:28:04 +0100

    Add enabled flag to patch metadata.

--- lib/data/filesystem/patches/patch_metadata.dart
+++ lib/data/filesystem/patches/patch_metadata.dart
@@ -6,12 +6,15 @@ class PatchMetadata {
   /// The name of the patch.  This is 1-30 characters of a-z, 0-9 and _.
   final String name;
 
+  /// When false, the patch will not be included when loading the game.
+  final bool enabled;
+
   static final RegExp _nameRegExp = RegExp(r"^[a-z0-9_]+$");
 
   /// Creates a description of an existing patch.  Throws an [ArgumentError]
   /// when the given [name] is empty, longer than 30 characters or contains
   /// characters other than a-z, 0-9 or _.
-  PatchMetadata(this.timestamp, this.name) {
+  PatchMetadata(this.timestamp, this.name, this.enabled) {
     if (name.isEmpty) {
       throw ArgumentError("Patch names cannot be empty.");
     }
@@ -30,7 +33,7 @@ class PatchMetadata {
 
   /// Creates a description of an existing patch from the name of its directory.
   /// Throws [ArgumentError] when the directory name cannot be parsed.
-  static PatchMetadata parse(String directoryName) {
+  static PatchMetadata parse(String directoryName, bool enabled) {
     if (!_directoryNameRegExp.hasMatch(directoryName)) {
       throw ArgumentError(
           "The given directory name is not formatted like a patch.");
@@ -45,11 +48,11 @@ class PatchMetadata {
 
     final name = directoryName.substring(23);
 
-    return PatchMetadata(timestamp, name);
+    return PatchMetadata(timestamp, name, enabled);
   }
 
   @override
-  bool operator ==(other) => other is PatchMetadata && timestamp == other.timestamp && name == other.name;
+  bool operator ==(other) => other is PatchMetadata && timestamp == other.timestamp && name == other.name && enabled == other.enabled;
 
   @override
   int get hashCode => timestamp;
--- test/data/filesystem/patches/patch_metadata_test.dart
+++ test/data/filesystem/patches/patch_metadata_test.dart
@@ -2,14 +2,31 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:maryland_game_engine/data/filesystem/patches/patch_metadata.dart';
 
 void main() {
-  test("equal", () {
+  test("equal disabled", () {
     const timestamp = -35345352974832;
     const name = "test_name_123";
 
-    final a = PatchMetadata(timestamp, name);
-    final b = PatchMetadata(timestamp, name);
+    final a = PatchMetadata(timestamp, name, false);
+    final b = PatchMetadata(timestamp, name, false);
 
+    expect(a.timestamp, equals(timestamp));
     expect(a.name, equals(name));
+    expect(a.enabled, isFalse);
+    expect(a, equals(b));
+    expect(a.hashCode, equals(b.hashCode));
+    expect(a.toString(), "-0000035345352974832 - test_name_123");
+  });
+
+  test("equal enabled", () {
+    const timestamp = -35345352974832;
+    const name = "test_name_123";
+
+    final a = PatchMetadata(timestamp, name, true);
+    final b = PatchMetadata(timestamp, name, true);
+
+    expect(a.timestamp, equals(timestamp));
+    expect(a.name, equals(name));
+    expect(a.enabled, isTrue);
     expect(a, equals(b));
     expect(a.hashCode, equals(b.hashCode));
     expect(a.toString(), "-0000035345352974832 - test_name_123");
@@ -18,8 +35,8 @@ void main() {
   test("unequal name", () {
     const timestamp = -35345352974832;
 
-    final a = PatchMetadata(timestamp, "test_name_1");
-    final b = PatchMetadata(timestamp, "test_name_2");
+    final a = PatchMetadata(timestamp, "test_name_1", false);
+    final b = PatchMetadata(timestamp, "test_name_2", false);
 
     expect(a, isNot(equals(b)));
   });
@@ -27,42 +44,62 @@ void main() {
   test("unequal timestamp", () {
     const name = "test_name_123";
 
-    final a = PatchMetadata(-35345352974832, name);
-    final b = PatchMetadata(-35345352974831, name);
+    final a = PatchMetadata(-35345352974832, name, false);
+    final b = PatchMetadata(-35345352974831, name, false);
+
+    expect(a, isNot(equals(b)));
+  });
+
+  test("unequal disabled enabled", () {
+    const timestamp = -35345352974832;
+    const name = "test_name_123";
+
+    final a = PatchMetadata(timestamp, name, false);
+    final b = PatchMetadata(timestamp, name, true);
+
+    expect(a, isNot(equals(b)));
+  });
+
+  test("unequal enabled disabled", () {
+    const timestamp = -35345352974832;
+    const name = "test_name_123";
+
+    final a = PatchMetadata(timestamp, name, true);
+    final b = PatchMetadata(timestamp, name, false);
 
     expect(a, isNot(equals(b)));
   });
 
   test("does not throw an error when the timestamp is at its minimum value", () {
-    final patchMetadata = PatchMetadata(-9223372036854775808, "test_name_123");
+    final patchMetadata = PatchMetadata(-9223372036854775808, "test_name_123", false);
 
     expect(patchMetadata.toString(), equals("-9223372036854775808 - test_name_123"));
   });
 
   test("does not throw an error when the timestamp is 0", () {
-    final patchMetadata = PatchMetadata(0, "test_name_123");
+    final patchMetadata = PatchMetadata(0, "test_name_123", false);
 
     expect(patchMetadata.toString(), equals("+0000000000000000000 - test_name_123"));
   });
 
   test("does not throw an error when the timestamp is at its maximum value", () {
-    final patchMetadata = PatchMetadata(9223372036854775807, "test_name_123");
+    final patchMetadata = PatchMetadata(9223372036854775807, "test_name_123", false);
 
     expect(patchMetadata.toString(), equals("+9223372036854775807 - test_name_123"));
   });
 
   test("does not throw an error when the name is of the minimum length", () {
-    expect(() => PatchMetadata(-35345352974832, "a"), returnsNormally);
+    expect(() => PatchMetadata(-35345352974832, "a", false), returnsNormally);
   });
 
   test("does not throw an error when the name is of the maximum length", () {
     expect(
-        () => PatchMetadata(-35345352974832, "abcdefghij__________0123456789"), returnsNormally);
+        () => PatchMetadata(-35345352974832, "abcdefghij__________0123456789", false), returnsNormally);
   });
 
   test("throws the expected exception when the name is empty", () {
     expect(
-        () => PatchMetadata(-35345352974832, ""),
+        () => PatchMetadata(-35345352974832, "", false),
         throwsA(predicate((e) =>
             e is ArgumentError && e.message == "Patch names cannot be empty.")));
   });
@@ -71,7 +108,7 @@ void main() {
       "throws the expected exception when the name is one character longer than permitted",
       () {
     expect(
-        () => PatchMetadata(-35345352974832, "abcdefghij___________0123456789"),
+        () => PatchMetadata(-35345352974832, "abcdefghij___________0123456789", false),
         throwsA(predicate((e) =>
             e is ArgumentError &&
             e.message == "Patch names cannot exceed 30 characters in length.")));
@@ -81,7 +118,7 @@ void main() {
       "throws the expected exception when the name is two characters longer than permitted",
       () {
     expect(
-        () => PatchMetadata(-35345352974832, "abcdefghij____________0123456789"),
+        () => PatchMetadata(-35345352974832, "abcdefghij____________0123456789", false),
         throwsA(predicate((e) =>
             e is ArgumentError &&
             e.message == "Patch names cannot exceed 30 characters in length.")));
@@ -91,7 +128,7 @@ void main() {
       "throws the expected exception when the name contains upper case letters",
       () {
     expect(
-        () => PatchMetadata(-35345352974832, "abcdefghij___A__0123456789"),
+        () => PatchMetadata(-35345352974832, "abcdefghij___A__0123456789", false),
         throwsA(predicate((e) =>
             e is ArgumentError &&
             e.message ==
@@ -100,7 +137,7 @@ void main() {
 
   test("throws the expected exception when the name contains spaces", () {
     expect(
-        () => PatchMetadata(-35345352974832, "abcdefghij___ __0123456789"),
+        () => PatchMetadata(-35345352974832, "abcdefghij___ __0123456789", false),
         throwsA(predicate((e) =>
             e is ArgumentError &&
             e.message ==
@@ -109,7 +146,7 @@ void main() {
 
   test("throws the expected exception when the name contains symbols", () {
     expect(
-        () => PatchMetadata(-35345352974832, "abcdefghij___\$__0123456789"),
+        () => PatchMetadata(-35345352974832, "abcdefghij___\$__0123456789", false),
         throwsA(predicate((e) =>
             e is ArgumentError &&
             e.message ==
@@ -118,7 +155,7 @@ void main() {
 
   test("throws the expected exception when the name contains new lines", () {
     expect(
-        () => PatchMetadata(-35345352974832, "abcdefghij___\n__0123456789"),
+        () => PatchMetadata(-35345352974832, "abcdefghij___\n__0123456789", false),
         throwsA(predicate((e) =>
             e is ArgumentError &&
             e.message ==
@@ -126,51 +163,57 @@ void main() {
   });
 
   group("parse", () {
-    test("can parse valid directory names", () {
-      final actual = PatchMetadata.parse("-0000035345352974832 - example_game_name_123");
+    test("can parse valid disabled directory names", () {
+      final actual = PatchMetadata.parse("-0000035345352974832 - example_game_name_123", false);
 
-      expect(actual, equals(PatchMetadata(-35345352974832, "example_game_name_123")));
+      expect(actual, equals(PatchMetadata(-35345352974832, "example_game_name_123", false)));
+    });
+
+    test("can parse valid enabled directory names", () {
+      final actual = PatchMetadata.parse("-0000035345352974832 - example_game_name_123", true);
+
+      expect(actual, equals(PatchMetadata(-35345352974832, "example_game_name_123", true)));
     });
 
     test("can parse directory names with the minimum permitted timestamp", () {
-      final actual = PatchMetadata.parse("-9223372036854775808 - example_game_name_123");
+      final actual = PatchMetadata.parse("-9223372036854775808 - example_game_name_123", false);
 
-      expect(actual, equals(PatchMetadata(-9223372036854775808, "example_game_name_123")));
+      expect(actual, equals(PatchMetadata(-9223372036854775808, "example_game_name_123", false)));
     });
 
     test("can parse directory names with the maximum permitted timestamp", () {
-      final actual = PatchMetadata.parse("+9223372036854775807 - example_game_name_123");
+      final actual = PatchMetadata.parse("+9223372036854775807 - example_game_name_123", false);
 
-      expect(actual, equals(PatchMetadata(9223372036854775807, "example_game_name_123")));
+      expect(actual, equals(PatchMetadata(9223372036854775807, "example_game_name_123", false)));
     });
 
     test("can parse directory names with a negative zero timestamp", () {
-      final actual = PatchMetadata.parse("-0000000000000000000 - example_game_name_123");
+      final actual = PatchMetadata.parse("-0000000000000000000 - example_game_name_123", false);
 
-      expect(actual, equals(PatchMetadata(0, "example_game_name_123")));
+      expect(actual, equals(PatchMetadata(0, "example_game_name_123", false)));
     });
 
     test("can parse directory names with a positive zero timestamp", () {
-      final actual = PatchMetadata.parse("+0000000000000000000 - example_game_name_123");
+      final actual = PatchMetadata.parse("+0000000000000000000 - example_game_name_123", false);
 
-      expect(actual, equals(PatchMetadata(0, "example_game_name_123")));
+      expect(actual, equals(PatchMetadata(0, "example_game_name_123", false)));
     });
 
     test("can parse directory names with a minimum length name", () {
-      final actual = PatchMetadata.parse("-0000035345352974832 - a");
+      final actual = PatchMetadata.parse("-0000035345352974832 - a", false);
 
-      expect(actual, equals(PatchMetadata(-35345352974832, "a")));
+      expect(actual, equals(PatchMetadata(-35345352974832, "a", false)));
     });
 
     test("can parse directory names with a maximum length name", () {
-      final actual = PatchMetadata.parse("-0000035345352974832 - abcdefghij__________0123456789");
+      final actual = PatchMetadata.parse("-0000035345352974832 - abcdefghij__________0123456789", false);
 
-      expect(actual, equals(PatchMetadata(-35345352974832, "abcdefghij__________0123456789")));
+      expect(actual, equals(PatchMetadata(-35345352974832, "abcdefghij__________0123456789", false)));
     });
 
     test("throws the expected exception when the timestamp is lacking a sign", () {
       expect(
-              () => PatchMetadata.parse("0000035345352974832 - abcdefghij__________0123456789"),
+              () => PatchMetadata.parse("0000035345352974832 - abcdefghij__________0123456789", false),
           throwsA(predicate((e) =>
           e is ArgumentError &&
               e.message ==
@@ -179,7 +222,7 @@ void main() {
 
     test("throws the expected exception when the timestamp has an invalid sign", () {
       expect(
-              () => PatchMetadata.parse("?0000035345352974832 - abcdefghij__________0123456789"),
+              () => PatchMetadata.parse("?0000035345352974832 - abcdefghij__________0123456789", false),
           throwsA(predicate((e) =>
           e is ArgumentError &&
               e.message ==
@@ -188,7 +231,7 @@ void main() {
 
     test("throws the expected exception when the timestamp is lacking a digit", () {
       expect(
-              () => PatchMetadata.parse("-000035345352974832 - abcdefghij__________0123456789"),
+              () => PatchMetadata.parse("-000035345352974832 - abcdefghij__________0123456789", false),
           throwsA(predicate((e) =>
           e is ArgumentError &&
               e.message ==
@@ -197,7 +240,7 @@ void main() {
 
     test("throws the expected exception when the timestamp has an excess digit", () {
       expect(
-              () => PatchMetadata.parse("-00000035345352974832 - abcdefghij__________0123456789"),
+              () => PatchMetadata.parse("-00000035345352974832 - abcdefghij__________0123456789", false),
           throwsA(predicate((e) =>
           e is ArgumentError &&
               e.message ==
@@ -206,7 +249,7 @@ void main() {
 
     test("throws the expected exception when the timestamp contains non-digits", () {
       expect(
-              () => PatchMetadata.parse("-000003a5345352974832 - abcdefghij__________0123456789"),
+              () => PatchMetadata.parse("-000003a5345352974832 - abcdefghij__________0123456789", false),
           throwsA(predicate((e) =>
           e is ArgumentError &&
               e.message ==
@@ -215,7 +258,7 @@ void main() {
 
     test("throws the expected exception when the timestamp is one less than possible", () {
       expect(
-              () => PatchMetadata.parse("-9223372036854775809 - abcdefghij__________0123456789"),
+              () => PatchMetadata.parse("-9223372036854775809 - abcdefghij__________0123456789", false),
           throwsA(predicate((e) =>
           e is ArgumentError &&
               e.message ==
@@ -224,7 +267,7 @@ void main() {
 
     test("throws the expected exception when the timestamp is two less than possible", () {
       expect(
-              () => PatchMetadata.parse("-9223372036854775810 - abcdefghij__________0123456789"),
+              () => PatchMetadata.parse("-9223372036854775810 - abcdefghij__________0123456789", false),
           throwsA(predicate((e) =>
           e is ArgumentError &&
               e.message ==
@@ -233,7 +276,7 @@ void main() {
 
     test("throws the expected exception when the timestamp is one greater than possible", () {
       expect(
-              () => PatchMetadata.parse("+9223372036854775808 - abcdefghij__________0123456789"),
+              () => PatchMetadata.parse("+9223372036854775808 - abcdefghij__________0123456789", false),
           throwsA(predicate((e) =>
           e is ArgumentError &&
               e.message ==
@@ -242,7 +285,7 @@ void main() {
 
     test("throws the expected exception when the timestamp is two greater than possible", () {
       expect(
-              () => PatchMetadata.parse("+9223372036854775809 - abcdefghij__________0123456789"),
+              () => PatchMetadata.parse("+9223372036854775809 - abcdefghij__________0123456789", false),
           throwsA(predicate((e) =>
           e is ArgumentError &&
               e.message ==
@@ -251,7 +294,7 @@ void main() {
 
     test("throws the expected exception when the space before the hyphen is missing", () {
       expect(
-              () => PatchMetadata.parse("-00000345345352974832- abcdefghij__________0123456789"),
+              () => PatchMetadata.parse("-00000345345352974832- abcdefghij__________0123456789", false),
           throwsA(predicate((e) =>
           e is ArgumentError &&
               e.message ==
@@ -260,7 +303,7 @@ void main() {
 
     test("throws the expected exception when the hyphen is missing", () {
       expect(
-              () => PatchMetadata.parse("-00000345345352974832  abcdefghij__________0123456789"),
+              () => PatchMetadata.parse("-00000345345352974832  abcdefghij__________0123456789", false),
           throwsA(predicate((e) =>
           e is ArgumentError &&
               e.message ==
@@ -269,7 +312,7 @@ void main() {
 
     test("throws the expected exception when the space after the hyphen is missing", () {
       expect(
-              () => PatchMetadata.parse("-00000345345352974832 -abcdefghij__________0123456789"),
+              () => PatchMetadata.parse("-00000345345352974832 -abcdefghij__________0123456789", false),
           throwsA(predicate((e) =>
           e is ArgumentError &&
               e.message ==
@@ -278,7 +321,7 @@ void main() {
 
     test("throws the expected exception when the name is empty", () {
       expect(
-              () => PatchMetadata.parse("-00000345345352974832 - "),
+              () => PatchMetadata.parse("-00000345345352974832 - ", false),
           throwsA(predicate((e) =>
           e is ArgumentError &&
               e.message ==
@@ -289,7 +332,7 @@ void main() {
         "throws the expected exception when the name is one character longer than permitted",
             () {
           expect(
-                  () => PatchMetadata.parse("-0000035345352974832 - abcdefghij___________0123456789"),
+                  () => PatchMetadata.parse("-0000035345352974832 - abcdefghij___________0123456789", false),
               throwsA(predicate((e) =>
               e is ArgumentError &&
                   e.message == "Patch names cannot exceed 30 characters in length.")));
@@ -299,7 +342,7 @@ void main() {
         "throws the expected exception when the name is two characters longer than permitted",
             () {
           expect(
-                  () => PatchMetadata.parse("-0000035345352974832 - abcdefghij____________0123456789"),
+                  () => PatchMetadata.parse("-0000035345352974832 - abcdefghij____________0123456789", false),
               throwsA(predicate((e) =>
               e is ArgumentError &&
                   e.message == "Patch names cannot exceed 30 characters in length.")));
@@ -309,7 +352,7 @@ void main() {
         "throws the expected exception when the name contains upper case letters",
             () {
           expect(
-                  () => PatchMetadata.parse("-0000035345352974832 - abcdefghij___A__0123456789"),
+                  () => PatchMetadata.parse("-0000035345352974832 - abcdefghij___A__0123456789", false),
               throwsA(predicate((e) =>
               e is ArgumentError &&
                   e.message ==
@@ -318,7 +361,7 @@ void main() {
 
     test("throws the expected exception when the name contains spaces", () {
       expect(
-              () => PatchMetadata.parse("-0000035345352974832 - abcdefghij___ __0123456789"),
+              () => PatchMetadata.parse("-0000035345352974832 - abcdefghij___ __0123456789", false),
           throwsA(predicate((e) =>
           e is ArgumentError &&
               e.message ==
@@ -327,7 +370,7 @@ void main() {
 
     test("throws the expected exception when the name contains symbols", () {
       expect(
-              () => PatchMetadata.parse("-0000035345352974832 - abcdefghij___\$__0123456789"),
+              () => PatchMetadata.parse("-0000035345352974832 - abcdefghij___\$__0123456789", false),
           throwsA(predicate((e) =>
           e is ArgumentError &&
               e.message ==
@@ -336,7 +379,7 @@ void main() {
 
     test("throws the expected exception when the name contains new lines", () {
       expect(
-              () => PatchMetadata.parse("-0000035345352974832 - abcdefghij___\n__0123456789"),
+              () => PatchMetadata.parse("-0000035345352974832 - abcdefghij___\n__0123456789", false),
           throwsA(predicate((e) =>
           e is ArgumentError &&
               e.message ==
