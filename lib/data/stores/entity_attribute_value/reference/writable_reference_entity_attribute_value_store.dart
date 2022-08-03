0915429f087913cbaaa10d534fc89d02673503a9
jameswilddev <jameswilddev@outlook.com>
2022-08-04 00:10:15 +0100

    Implement reference stores.

---
+++ lib/data/stores/entity_attribute_value/reference/writable_reference_entity_attribute_value_store.dart
@@ -0,0 +1,148 @@
+import 'package:maryland_game_engine/data/stores/entity_attribute_value/reference/reference_entity_attribute_value_store.dart';
+
+import '../../../primitives/attribute_id.dart';
+import '../../../primitives/entity_id.dart';
+
+/// An entity-attribute-value reference store which can be written to.
+class WritableReferenceEntityAttributeValueStore implements ReferenceEntityAttributeValueStore {
+  /// The store which is read from when this store lacks a mapping.
+  final ReferenceEntityAttributeValueStore backingStore;
+
+  final Map<EntityId, Map<AttributeId, EntityId>> _forward = <EntityId, Map<AttributeId, EntityId>>{};
+  final Map<AttributeId, Map<EntityId, Set<EntityId>>> _inverse = <AttributeId, Map<EntityId, Set<EntityId>>>{};
+
+  /// Creates a new entity-attribute-value reference store which can be written
+  /// to, with a given [backingStore] to be used when no match can be found
+  /// during a lookup.
+  WritableReferenceEntityAttributeValueStore(this.backingStore);
+
+  /// Retrieves the value of the specified [entityId]/[attributeId] pair.  May
+  /// fall back to the [backingStore] if this store lacks such a mapping.
+  /// Throws a [RangeError] when the given [attributeId] is invalid.
+  @override
+  EntityId get(EntityId entityId, AttributeId attributeId) {
+    validateAttributeId(attributeId, "Attribute ID");
+
+    return _get(entityId, attributeId) ?? backingStore.get(entityId, attributeId);
+  }
+
+  EntityId? _get(EntityId entityId, AttributeId attributeId) {
+    final valuesByAttributes = _forward[entityId];
+
+    if (valuesByAttributes == null) {
+      return null;
+    }
+
+    final output = valuesByAttributes[attributeId];
+
+    if (output == null) {
+      return null;
+    }
+
+    return output;
+  }
+
+  /// Lists all [EntityId]s which refer to a given [entityId] using a specified
+  /// [attributeId].  Throws a [RangeError] when the given [attributeId] is
+  /// invalid.
+  @override
+  Iterable<EntityId> listReferencingEntities(AttributeId attributeId, EntityId entityId) sync* {
+    final referencingEntityIdsByEntityIds = _inverse[attributeId];
+
+    if (referencingEntityIdsByEntityIds == null) {
+      for (final referencingEntityId in backingStore.listReferencingEntities(attributeId, entityId)) {
+        if (_get(referencingEntityId, attributeId) == null) {
+          yield referencingEntityId;
+        }
+      }
+
+      return;
+    }
+
+    final referencingEntityIds = referencingEntityIdsByEntityIds[entityId];
+
+    if (referencingEntityIds == null) {
+      for (final referencingEntityId in backingStore.listReferencingEntities(attributeId, entityId)) {
+        if (_get(referencingEntityId, attributeId) == null) {
+          yield referencingEntityId;
+        }
+      }
+
+      return;
+    }
+
+    yield* referencingEntityIds;
+
+    for (final referencingEntityId in backingStore.listReferencingEntities(attributeId, entityId)) {
+      if (! referencingEntityIds.contains(referencingEntityId)) {
+        if (_get(referencingEntityId, attributeId) == null) {
+          yield referencingEntityId;
+        }
+      }
+    }
+  }
+
+  /// Sets of overwrites the [value] of the specified
+  set(EntityId entityId, AttributeId attributeId, EntityId value) {
+    validateAttributeId(attributeId, "Attribute ID");
+
+    final valuesByAttributes = _forward[entityId];
+
+    if (valuesByAttributes == null) {
+      _forward[entityId] = { attributeId: value };
+    } else {
+      final existingValue = valuesByAttributes[attributeId];
+
+      if (existingValue == value) {
+        return;
+      }
+
+      valuesByAttributes[attributeId] = value;
+
+      if (existingValue != null) {
+        var entitiesByEntities = _inverse[attributeId]!;
+        var entities = entitiesByEntities[existingValue]!;
+
+        if (entities.length == 1) {
+         entitiesByEntities.remove(existingValue);
+        } else {
+         entities.remove(entityId);
+        }
+      }
+    }
+
+    var entitiesByEntities = _inverse[attributeId];
+
+    if (entitiesByEntities == null) {
+      entitiesByEntities = {};
+      _inverse[attributeId] = entitiesByEntities;
+    }
+
+    var entities = entitiesByEntities[value];
+
+    if (entities == null) {
+      entities = <EntityId>{};
+      entitiesByEntities[value] = entities;
+    }
+
+    entities.add(entityId);
+  }
+
+  /// Applies all entity-attribute-values within this store to a given [store],
+  /// then clears this store.
+  moveTo(WritableReferenceEntityAttributeValueStore store) {
+    for (final entityAttributeValue in _forward.entries) {
+      final entityId = entityAttributeValue.key;
+
+      for (final attributeValue in entityAttributeValue.value.entries) {
+        final attributeId = attributeValue.key;
+        final value = attributeValue.value;
+
+        store.set(entityId, attributeId, value);
+      }
+    }
+
+    _forward.clear();
+    _inverse.clear();
+  }
+}
---
+++ test/data/stores/entity_attribute_value/reference/writable_reference_entity_attribute_value_store_test.dart
@@ -0,0 +1,263 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:maryland_game_engine/data/primitives/entity_id.dart';
+import 'package:maryland_game_engine/data/stores/entity_attribute_value/reference/reference_entity_attribute_value_store.dart';
+import 'package:maryland_game_engine/data/stores/entity_attribute_value/reference/writable_reference_entity_attribute_value_store.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+@GenerateMocks([ReferenceEntityAttributeValueStore, WritableReferenceEntityAttributeValueStore])
+import 'writable_reference_entity_attribute_value_store_test.mocks.dart';
+
+void main() {
+  test("exposes the backing store", () {
+    final backingStore = MockReferenceEntityAttributeValueStore();
+
+    final store = WritableReferenceEntityAttributeValueStore(backingStore);
+
+    expect(store.backingStore, equals(backingStore));
+    verifyZeroInteractions(backingStore);
+  });
+
+
+  test("get throws an error when an invalid attribute is given", () {
+    final backingStore = MockReferenceEntityAttributeValueStore();
+    final store = WritableReferenceEntityAttributeValueStore(backingStore);
+
+    expect(
+            () => store.get(EntityId.generate(), -1),
+        throwsA((e) => e is RangeError && e.message == "Attribute ID - Value is out of range for an attribute ID (less than 0).")
+    );
+    verifyZeroInteractions(backingStore);
+  });
+
+  test("set throws an error when an invalid attribute is given", () {
+    final backingStore = MockReferenceEntityAttributeValueStore();
+    final store = WritableReferenceEntityAttributeValueStore(backingStore);
+
+    expect(
+            () => store.set(EntityId.generate(), -1, EntityId.generate()),
+        throwsA((e) => e is RangeError && e.message == "Attribute ID - Value is out of range for an attribute ID (less than 0).")
+    );
+    verifyZeroInteractions(backingStore);
+  });
+
+  test("listReferencingEntities throws an error when an invalid attribute is given", () {
+    final backingStore = MockReferenceEntityAttributeValueStore();
+    final store = WritableReferenceEntityAttributeValueStore(backingStore);
+
+    expect(
+            () => store.listReferencingEntities(-1, EntityId.generate()),
+        throwsA((e) => e is RangeError && e.message == "Attribute ID - Value is out of range for an attribute ID (less than 0).")
+    );
+    verifyZeroInteractions(backingStore);
+  });
+
+  test("allows retrieval of set values", () {
+    final entityAId = EntityId.generate();
+    final entityBId = EntityId.generate();
+    final entityCId = EntityId.generate();
+    final entityDId = EntityId.generate();
+    final entityEId = EntityId.generate();
+    final entityFId = EntityId.generate();
+    final entityGId = EntityId.generate();
+    final entityHId = EntityId.generate();
+    final entityIId = EntityId.generate();
+    final entityJId = EntityId.generate();
+    final entityKId = EntityId.generate();
+    final entityLId = EntityId.generate();
+    final entityMId = EntityId.generate();
+    final entityNId = EntityId.generate();
+    final entityOId = EntityId.generate();
+    final entityPId = EntityId.generate();
+    final entityQId = EntityId.generate();
+    final entityRId = EntityId.generate();
+    final entitySId = EntityId.generate();
+    final entityTId = EntityId.generate();
+    final entityUId = EntityId.generate();
+    const attributeAId = 1234;
+    const attributeBId = 5959;
+    const attributeCId = 2211;
+    final backingStore = MockReferenceEntityAttributeValueStore();
+    when(backingStore.get(entityAId, attributeCId)).thenReturn(entityFId);
+    when(backingStore.get(entityGId, attributeAId)).thenReturn(entityHId);
+    when(backingStore.listReferencingEntities(attributeBId, entityDId)).thenReturn([entityAId, entityKId, entityJId, entityQId]);
+    when(backingStore.listReferencingEntities(attributeBId, entityMId)).thenReturn([entityNId, entityOId]);
+    when(backingStore.listReferencingEntities(attributeBId, entityTId)).thenReturn([]);
+    final writableReferenceEntityAttributeValueStore = WritableReferenceEntityAttributeValueStore(backingStore);
+
+    writableReferenceEntityAttributeValueStore.set(entityAId, attributeBId, entityCId);
+    writableReferenceEntityAttributeValueStore.set(entityAId, attributeAId, entityBId);
+    writableReferenceEntityAttributeValueStore.set(entityAId, attributeBId, entityDId);
+    writableReferenceEntityAttributeValueStore.set(entityEId, attributeAId, entityBId);
+    writableReferenceEntityAttributeValueStore.set(entityIId, attributeBId, entityDId);
+    writableReferenceEntityAttributeValueStore.set(entityKId, attributeBId, entityLId);
+    writableReferenceEntityAttributeValueStore.set(entityOId, attributeBId, entityPId);
+    writableReferenceEntityAttributeValueStore.set(entityQId, attributeBId, entityDId);
+    writableReferenceEntityAttributeValueStore.set(entityQId, attributeBId, entityRId);
+    writableReferenceEntityAttributeValueStore.set(entitySId, attributeBId, entityTId);
+    writableReferenceEntityAttributeValueStore.set(entitySId, attributeBId, entityUId);
+    final valueAA = writableReferenceEntityAttributeValueStore.get(entityAId, attributeAId);
+    final valueAB = writableReferenceEntityAttributeValueStore.get(entityAId, attributeBId);
+    final valueEA = writableReferenceEntityAttributeValueStore.get(entityEId, attributeAId);
+    final valueAC = writableReferenceEntityAttributeValueStore.get(entityAId, attributeCId);
+    final valueGA = writableReferenceEntityAttributeValueStore.get(entityGId, attributeAId);
+    final referencesBD = writableReferenceEntityAttributeValueStore.listReferencingEntities(attributeBId, entityDId);
+    final referencesBM = writableReferenceEntityAttributeValueStore.listReferencingEntities(attributeBId, entityMId);
+    final referencesBT = writableReferenceEntityAttributeValueStore.listReferencingEntities(attributeBId, entityTId);
+
+    expect(valueAA, equals(entityBId));
+    expect(valueAB, equals(entityDId));
+    expect(valueEA, equals(entityBId));
+    expect(valueAC, equals(entityFId));
+    expect(valueGA, equals(entityHId));
+    expect(referencesBD, unorderedEquals([entityAId, entityIId, entityJId]));
+    expect(referencesBM, unorderedEquals([entityNId]));
+    expect(referencesBT, isEmpty);
+    verify(backingStore.get(entityAId, attributeCId)).called(1);
+    verify(backingStore.get(entityGId, attributeAId)).called(1);
+    verify(backingStore.listReferencingEntities(attributeBId, entityDId)).called(1);
+    verify(backingStore.listReferencingEntities(attributeBId, entityMId)).called(1);
+    verify(backingStore.listReferencingEntities(attributeBId, entityTId)).called(1);
+    verifyNoMoreInteractions(backingStore);
+  });
+
+  group("moveTo", () {
+    test("applies changes", () {
+      final entityAId = EntityId.generate();
+      final entityBId = EntityId.generate();
+      final entityCId = EntityId.generate();
+      final entityDId = EntityId.generate();
+      final entityEId = EntityId.generate();
+      final entityIId = EntityId.generate();
+      final entityKId = EntityId.generate();
+      final entityLId = EntityId.generate();
+      final entityOId = EntityId.generate();
+      final entityPId = EntityId.generate();
+      final entityQId = EntityId.generate();
+      final entityRId = EntityId.generate();
+      final entitySId = EntityId.generate();
+      final entityTId = EntityId.generate();
+      final entityUId = EntityId.generate();
+      const attributeAId = 1234;
+      const attributeBId = 5959;
+      final backingStore = MockReferenceEntityAttributeValueStore();
+      final writableReferenceEntityAttributeValueStore = WritableReferenceEntityAttributeValueStore(backingStore);
+      final target = MockWritableReferenceEntityAttributeValueStore();
+
+      writableReferenceEntityAttributeValueStore.moveTo(target);
+
+      writableReferenceEntityAttributeValueStore.set(entityAId, attributeBId, entityCId);
+      writableReferenceEntityAttributeValueStore.set(entityAId, attributeAId, entityBId);
+      writableReferenceEntityAttributeValueStore.set(entityAId, attributeBId, entityDId);
+      writableReferenceEntityAttributeValueStore.set(entityEId, attributeAId, entityBId);
+      writableReferenceEntityAttributeValueStore.set(entityIId, attributeBId, entityDId);
+      writableReferenceEntityAttributeValueStore.set(entityKId, attributeBId, entityLId);
+      writableReferenceEntityAttributeValueStore.set(entityOId, attributeBId, entityPId);
+      writableReferenceEntityAttributeValueStore.set(entityQId, attributeBId, entityDId);
+      writableReferenceEntityAttributeValueStore.set(entityQId, attributeBId, entityRId);
+      writableReferenceEntityAttributeValueStore.set(entitySId, attributeBId, entityTId);
+      writableReferenceEntityAttributeValueStore.set(entitySId, attributeBId, entityUId);
+      verifyZeroInteractions(backingStore);
+      verify(writableReferenceEntityAttributeValueStore.set(entityAId, attributeBId, entityCId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entityAId, attributeAId, entityBId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entityAId, attributeBId, entityDId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entityEId, attributeAId, entityBId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entityIId, attributeBId, entityDId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entityKId, attributeBId, entityLId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entityOId, attributeBId, entityPId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entityQId, attributeBId, entityDId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entityQId, attributeBId, entityRId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entitySId, attributeBId, entityTId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entitySId, attributeBId, entityUId)).called(1);
+      verifyNoMoreInteractions(target);
+    });
+
+    test("resets", () {
+      final entityAId = EntityId.generate();
+      final entityBId = EntityId.generate();
+      final entityCId = EntityId.generate();
+      final entityDId = EntityId.generate();
+      final entityEId = EntityId.generate();
+      final entityFId = EntityId.generate();
+      final entityGId = EntityId.generate();
+      final entityHId = EntityId.generate();
+      final entityIId = EntityId.generate();
+      final entityJId = EntityId.generate();
+      final entityKId = EntityId.generate();
+      final entityLId = EntityId.generate();
+      final entityMId = EntityId.generate();
+      final entityNId = EntityId.generate();
+      final entityOId = EntityId.generate();
+      final entityPId = EntityId.generate();
+      final entityQId = EntityId.generate();
+      final entityRId = EntityId.generate();
+      final entitySId = EntityId.generate();
+      final entityTId = EntityId.generate();
+      final entityUId = EntityId.generate();
+      final entityVId = EntityId.generate();
+      final entityWId = EntityId.generate();
+      final entityXId = EntityId.generate();
+      const attributeAId = 1234;
+      const attributeBId = 5959;
+      const attributeCId = 2211;
+      final backingStore = MockReferenceEntityAttributeValueStore();
+      when(backingStore.get(entityAId, attributeAId)).thenReturn(entityVId);
+      when(backingStore.get(entityAId, attributeBId)).thenReturn(entityWId);
+      when(backingStore.get(entityEId, attributeAId)).thenReturn(entityXId);
+      when(backingStore.get(entityAId, attributeCId)).thenReturn(entityFId);
+      when(backingStore.get(entityGId, attributeAId)).thenReturn(entityHId);
+      when(backingStore.listReferencingEntities(attributeBId, entityDId)).thenReturn([entityAId, entityKId, entityJId, entityQId]);
+      when(backingStore.listReferencingEntities(attributeBId, entityMId)).thenReturn([entityNId, entityOId]);
+      when(backingStore.listReferencingEntities(attributeBId, entityTId)).thenReturn([]);
+      final writableReferenceEntityAttributeValueStore = WritableReferenceEntityAttributeValueStore(backingStore);
+      final target = MockWritableReferenceEntityAttributeValueStore();
+
+      writableReferenceEntityAttributeValueStore.moveTo(target);
+
+      writableReferenceEntityAttributeValueStore.set(entityAId, attributeBId, entityCId);
+      writableReferenceEntityAttributeValueStore.set(entityAId, attributeAId, entityBId);
+      writableReferenceEntityAttributeValueStore.set(entityAId, attributeBId, entityDId);
+      writableReferenceEntityAttributeValueStore.set(entityEId, attributeAId, entityBId);
+      writableReferenceEntityAttributeValueStore.set(entityIId, attributeBId, entityDId);
+      writableReferenceEntityAttributeValueStore.set(entityKId, attributeBId, entityLId);
+      writableReferenceEntityAttributeValueStore.set(entityOId, attributeBId, entityPId);
+      writableReferenceEntityAttributeValueStore.set(entityQId, attributeBId, entityDId);
+      writableReferenceEntityAttributeValueStore.set(entityQId, attributeBId, entityRId);
+      writableReferenceEntityAttributeValueStore.set(entitySId, attributeBId, entityTId);
+      writableReferenceEntityAttributeValueStore.set(entitySId, attributeBId, entityUId);
+      final valueAA = writableReferenceEntityAttributeValueStore.get(entityAId, attributeAId);
+      final valueAB = writableReferenceEntityAttributeValueStore.get(entityAId, attributeBId);
+      final valueEA = writableReferenceEntityAttributeValueStore.get(entityEId, attributeAId);
+      final valueAC = writableReferenceEntityAttributeValueStore.get(entityAId, attributeCId);
+      final valueGA = writableReferenceEntityAttributeValueStore.get(entityGId, attributeAId);
+      final referencesBD = writableReferenceEntityAttributeValueStore.listReferencingEntities(attributeBId, entityDId);
+      final referencesBM = writableReferenceEntityAttributeValueStore.listReferencingEntities(attributeBId, entityMId);
+      final referencesBT = writableReferenceEntityAttributeValueStore.listReferencingEntities(attributeBId, entityTId);
+      expect(valueAA, equals(entityVId));
+      expect(valueAB, equals(entityWId));
+      expect(valueEA, equals(entityXId));
+      expect(valueAC, equals(entityFId));
+      expect(valueGA, equals(entityHId));
+      expect(referencesBD, unorderedEquals([entityAId, entityKId, entityJId, entityQId]));
+      expect(referencesBM, unorderedEquals([entityNId, entityOId]));
+      expect(referencesBT, isEmpty);
+      verify(backingStore.get(entityAId, attributeCId)).called(1);
+      verify(backingStore.get(entityGId, attributeAId)).called(1);
+      verify(backingStore.listReferencingEntities(attributeBId, entityDId)).called(1);
+      verify(backingStore.listReferencingEntities(attributeBId, entityMId)).called(1);
+      verify(backingStore.listReferencingEntities(attributeBId, entityTId)).called(1);
+      verifyNoMoreInteractions(backingStore);
+      verify(writableReferenceEntityAttributeValueStore.set(entityAId, attributeBId, entityCId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entityAId, attributeAId, entityBId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entityAId, attributeBId, entityDId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entityEId, attributeAId, entityBId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entityIId, attributeBId, entityDId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entityKId, attributeBId, entityLId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entityOId, attributeBId, entityPId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entityQId, attributeBId, entityDId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entityQId, attributeBId, entityRId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entitySId, attributeBId, entityTId)).called(1);
+      verify(writableReferenceEntityAttributeValueStore.set(entitySId, attributeBId, entityUId)).called(1);
+    });
+  });
+}
